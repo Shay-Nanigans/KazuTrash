6adc5d2eb37d574be62717da6b0174421aaf3598
Shay Koko-Smith <shay.smith314@gmail.com>
2024-07-12 13:49:34 -0400

    Fixing some bugs in vidclip, accepts ytlink?t=startime endtime, can change speed, crop and rename

--- cogs/YTCog.py
+++ cogs/YTCog.py
@@ -2,9 +2,10 @@ import asyncio
 import discord
 import os
 import yt_dlp
-from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip,vfx
 import ytcaptionfinder
 import random
+import re
 
 
 from discord.ext import commands
@@ -13,8 +14,18 @@ class YTCog(commands.Cog):
         self.bot = bot
         self._last_member = None
 
-    @commands.command(name="ytclip", help = "Makes clip from youtube video. $ytclip url start end")
-    async def ytclip(self, ctx, url, start:str, end:str):
+    @commands.command(name="ytclip")
+    async def ytclip(self, ctx, url, *args):
+        """Makes clip from youtube video. 
+        $ytclip url start end
+        -speed [multiplier]: changes clip playback speed"""
+        
+        if "?t=" in url:
+            end = args[0]
+            url, start = url.split("?t=",1)
+        else:
+            end = args[1]
+            start = args[0]
 
         #seconds or minutes:seconds or hour:minutes:seconds or something
         if start.count(":") == 2:
@@ -37,26 +48,81 @@ class YTCog(commands.Cog):
             return
 
         ydl_opts = {
-                    'format': 'mp4',
                     'outtmpl': os.path.join(os.getcwd(),'vidclipcogtemp/%(id)s.%(ext)s'),
                 }
         async with ctx.channel.typing(): #worse loading bar
             id = yt_dlp.YoutubeDL(ydl_opts).extract_info(url)['id'] #fetch
             clip = VideoFileClip(f"vidclipcogtemp/{id}.mp4").subclip(start, end) #shrink
-            clip.to_videofile(f"vidclipcogtemp/{id}_{start}-{end}.mp4", codec="libx264", temp_audiofile=f'vidclipcogtemp/{id}temp-audio.m4a', remove_temp=True, audio_codec='aac') #encode
+            #change speed
+            # print(str(' '.join(args)))
+            try:
+                speed = re.search(r"-speed (\d*\.*\d+)", str(' '.join(args))) 
+                print(speed)
+                namespeed=""
+                if speed:
+                    speed = float(speed.group(1))
+                    print(speed)
+                    clip = clip.fx(vfx.speedx, speed)
+                    namespeed = f"_x{speed}"
+            except Exception as e:
+                print(e)
+            try:
+                noaudio = re.search(r"-noaudio", str(' '.join(args))) 
+                if noaudio:
+                    clip = clip.without_audio()
+            except Exception as e:
+                print(e)
+                
+            try:
+                namesize = ""
+                if "-cropx" in args or "-cropy" in args:
+                    size = clip.size
+                    corners = [0,size[0],0,size[1]] # x1 x2 y1 y2
+
+                    cropx = re.search(r"-cropx (\d*\.*\d+) (\d*\.*\d+)", str(' '.join(args))) 
+                    print(cropx)
+                    if cropx:
+                        cropx = (float(cropx.group(1)),float(cropx.group(2)))
+                        corners[0] = int(cropx[0]*size[0]/100)
+                        corners[1] = int(cropx[1]*size[0]/100)
+
+                    cropy = re.search(r"-cropy (\d*\.*\d+) (\d*\.*\d+)", str(' '.join(args))) 
+                    print(cropy)
+                    if cropy:
+                        cropy = (float(cropy.group(1)),float(cropy.group(2)))
+                        corners[3] = int(size[1] - cropy[0]*size[1]/100)
+                        corners[2] = int(size[1] - cropy[1]*size[1]/100)
+                    print(corners)
+                    clip  = vfx.crop(clip, x1=corners[0], x2=corners[1], y1=corners[2], y2=corners[3])
+                    namesize = f"_cropped_{corners[0]}-{corners[1]}_{corners[2]}-{corners[3]}"
+            except Exception as e:
+                print(e)
+
+            try:
+                filename = re.search(r"-name (\w*)", str(' '.join(args))) 
+                print(filename)
+            except Exception as e:
+                print(e)
+
+            if not filename:
+                clipname = f"vidclipcogtemp/{id}_{start}-{end}{namespeed}{namesize}.mp4"
+            else:
+                clipname = f'{filename.group(1)}.mp4'
+            vbitrate = f'{56000/(end-start)}k'
+            clip.write_videofile(clipname, codec="libx264", bitrate = vbitrate, temp_audiofile=f'vidclipcogtemp/{id}temp-audio.m4a', remove_temp=True, audio_codec='aac') #encode
 
             #too big
-            if os.stat(f"vidclipcogtemp/{id}_{start}-{end}.mp4").st_size > 8300000: 
+            if os.stat(clipname).st_size > 8300000: 
                 await ctx.send("Video larger than 8MB!")
-                os.remove(f"vidclipcogtemp/{id}_{start}-{end}.mp4")
+                os.remove(clipname)
                 clip.close()
                 os.remove(f"vidclipcogtemp/{id}.mp4")
                 return
 
-            await ctx.send(file=discord.File(f"vidclipcogtemp/{id}_{start}-{end}.mp4"))
+            await ctx.send(file=discord.File(clipname))
 
             #cleanup
-            os.remove(f"vidclipcogtemp/{id}_{start}-{end}.mp4")
+            os.remove(clipname)
             clip.close()
             os.remove(f"vidclipcogtemp/{id}.mp4")
 
